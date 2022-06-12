b33b18e08f067b160d68676c32dcefdd6a7d9c35
navitbranin <navitbranin123@gmail.com>
2022-06-12 09:01:04 +0300

    first

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -18,8 +18,6 @@ async function getRecipeInformation(recipe_id) {
     });
 }
 
-
-
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
     let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
@@ -36,8 +34,6 @@ async function getRecipeDetails(recipe_id) {
         
     }
 }
-
-
 
 exports.getRecipeDetails = getRecipeDetails;
 
